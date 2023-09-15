95a7b4b2f75dfc3ae1d88a934bc13345610b9d01
Kunj Pathak <kpathak3@dons.usfca.edu>
2023-09-15 00:01:59 -0700

    Add existing file

---
+++ Makefile
@@ -0,0 +1,11 @@
+OBJS = passwords.o project01.o sha256.o
+PROG = project01
+
+%.o %.c
+	gcc -c -g -o $@ $<
+
+$(PROG) : $(OBJS)
+	gcc -g -o $@ $^
+
+clean:
+	rm -rf $(PROG) $(OBJS)
--- project01.c
+++ project01.c
@@ -5,9 +5,13 @@
 
 #include "passwords.h"
 #include "sha256.h"
+#include "sha256.c" 
+#include "passwords.c"
 
 #define DIG_BIN_LEN 32
 #define DIG_STR_LEN ((DIG_BIN_LEN * 2) + 1)
+
+#define  PASSWORD_LENGTH 64;
 
 void sha256(char *dest, char *src) {
     // zero out the sha256 context
@@ -32,3 +36,111 @@ void sha256(char *dest, char *src) {
         dest += 2;
     }
 }
+
+
+
+char change(char curr)
+{
+    switch (curr)
+    {
+    case 'o':
+        return '0';
+    case 'e':
+        return '3';
+    case 'i':
+        return '!';
+    case 'a':
+        return '@';
+    case 'h':
+        return '#';
+    case 's':
+        return '$';
+    case 't':
+        return '+';
+    }
+    return curr;
+}
+
+void change_password(char *password)
+{
+    for (int i = 0; i < strlen(password); i++)
+    {
+        password[i] = change(password[i]);
+    }
+}
+
+void add1(char *password)
+{
+    strcat(password, "1");
+}
+
+int main(int argc, const char **argv)
+{
+    if (argc != 2)
+    {
+        perror("Usage: ./project01 <password>");
+        return 1;
+    }
+
+ 	char *given_hash = (char *)argv[1];
+	char *hash = malloc(DIG_STR_LEN);
+	char *password = malloc(256);
+
+  // compare the given hash to the hash of each password
+	for (int i = 0; i < 10000; i++)
+	{
+     	sha256(hash, passwords[i]);
+        if (strcmp(hash, given_hash) == 0)
+ 		 {
+	      	strcpy(password, passwords[i]);
+            break;
+         }
+    }
+
+   // if the password is not found, try changing the password
+   // using l335t speak
+    if (password[0] == '\0')
+    {
+    	for (int i = 0; i < 10000; i++)
+        {
+           	 strcpy(password, passwords[i]);
+        	 change_password(password);
+        	 sha256(hash, password);
+
+             if (strcmp(hash, given_hash) == 0)
+             {
+				 break;
+             }
+	          password[0] = '\0';
+         }
+    }
+
+  // if the password is not found, try adding a 1 to the end
+    if (password[0] == '\0')
+     {
+      	for (int i = 0; i < 10000; i++)
+        {
+         	strcpy(password, passwords[i]);
+         	add1(password);
+            sha256(hash, password);
+
+            if (strcmp(hash, given_hash) == 0)
+            {
+             	break;
+             }
+
+            password[0] = '\0';
+         }
+      }
+            
+    if (password[0] == '\0')
+    {
+     	printf("not found\n");
+       return 1;
+    }
+    printf(password);
+
+    return 0;
+}
+            
+
